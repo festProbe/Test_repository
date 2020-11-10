d5b81c336ac9d31f8bc8bce99282c7094107ca97
unknown <festProbe@gmail.com>
2020-11-10 20:28:15 +1100

    first_react_app

---
+++ react-app01/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            {/*TODO*/}
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRCvMC35mZ9zwoLonZb_iOSc_gQFccPGB729g&usqp=CAU" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
